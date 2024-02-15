9158f3a7895769fb98cbc5a17a4f79fc09f1da6d
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2024-02-15 22:51:33 +0900

    feat: stylex 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import styleX from 'vite-plugin-stylex';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [svelte(), styleX()],
+});
