4d3ad1d443bf4aedd77893611af4f0d0959c5eac
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2024-02-16 23:00:33 +0900

    feat: 폴리곤 구현 중

--- src/components/canvas/Canvas.svelte
+++ src/components/canvas/Canvas.svelte
@@ -1,5 +1,18 @@
+<script context="module" lang="ts">
+  import * as stylex from "@stylexjs/stylex";
+
+  const styles = stylex.create({
+    base: {
+      position: "absolute",
+      left: 0,
+      top: 0,
+      zIndex: 2,
+    },
+  });
+</script>
+
 <script lang="ts">
-  import { onMount } from "svelte";
+  import { afterUpdate, onMount } from "svelte";
   import type { Size } from "../../types/Size";
   import type { Offset } from "../../types/Offset";
   import type { Position } from "../../types/Position";
@@ -13,28 +26,33 @@
   onMount(() => {
     context = canvas.getContext("2d");
     if (!context) return;
-    context.lineWidth = 3;
   });
 
-  $: if (context) {
-    context.strokeStyle = "black";
-  }
+  afterUpdate(() => {
+    handleSize();
+  });
 
   const handleStart = ({ offsetX: x, offsetY: y }: Offset) => {
     isDrawing = true;
     start = { x, y };
+    if (!context) return;
+    context.beginPath();
+    context.arc(x, y, 4, 0, 2 * Math.PI);
+    context.fillStyle = "pink";
+    context.fill();
+    context.stroke();
   };
 
   const handleMove = ({ offsetX: x1, offsetY: y1 }: Offset) => {
     if (!context) return;
     if (!isDrawing) return;
 
-    const { x, y } = start;
-    context.beginPath();
-    context.moveTo(x, y);
-    context.lineTo(x1, y1);
-    context.closePath();
-    context.stroke();
+    // const { x, y } = start;
+    // context.beginPath();
+    // context.moveTo(x, y);
+    // context.lineTo(x1, y1);
+    // context.closePath();
+    // context.stroke();
 
     start = { x: x1, y: y1 };
   };
@@ -43,13 +61,14 @@
     isDrawing = false;
   };
   const handleSize = () => {
+    if (!size) return;
     canvas.width = size.width;
     canvas.height = size.height;
   };
 </script>
 
-<svelte:window on:load="{handleSize}" on:resize="{handleSize}" />
 <canvas
+  {...stylex.attrs(styles.base)}
   bind:this="{canvas}"
   on:mousedown="{handleStart}"
   on:mousemove="{handleMove}"
--- src/components/canvas/index.svelte
+++ src/components/canvas/index.svelte
@@ -4,23 +4,27 @@
   const styles = stylex.create({
     base: {
       flex: 1,
+      position: "relative",
     },
   });
 </script>
 
 <script lang="ts">
   import Canvas from "./Canvas.svelte";
-  import { onMount } from "svelte";
   import type { Size } from "../../types/Size";
+  import ImageCanvas from "./ImageCanvas.svelte";
 
   let wrap: HTMLDivElement;
   let size: Size;
 
-  onMount(() => {
+  const handleSize = () => {
     size = { width: wrap.offsetWidth, height: wrap.offsetHeight };
-  });
+  };
 </script>
+
+<svelte:window on:load="{handleSize}" on:resize="{handleSize}" />
 
 <div {...stylex.attrs(styles.base)} bind:this="{wrap}">
   <Canvas size="{size}" />
+  <ImageCanvas size="{size}" />
 </div>
